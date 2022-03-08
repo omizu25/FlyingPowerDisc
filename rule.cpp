ae4ff265abdd59018edb1657d6341155abc5e4b9
student <student@S-JOBI21-013>
2022-03-09 08:43:38 +0900

    rule.cpp、hの基盤の追加

---
+++ rule.cpp
@@ -0,0 +1,189 @@
+//================================
+//
+// ルール選択画面の処理
+// Author:Sato Teruto
+//
+//================================
+
+//インクルード
+#include "rule.h"
+#include "main.h"
+
+//マクロ定義
+#define MAX_RULE	(3)		//ルールの最大数
+
+//スタティック変数
+static LPDIRECT3DTEXTURE9		s_pTexture = NULL;	//テクスチャへのポインタ
+static LPDIRECT3DVERTEXBUFFER9	s_pVtxBuff = NULL;	//頂点バッファへのポインタ
+static Rule s_Rule[MAX_RULE];	//構造体の取得
+
+//============================
+// ルール選択画面の初期化
+//============================
+void InitRule(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();	//デバイスの取得
+
+	//------------------------------
+	//	テクスチャの読み込み
+	//------------------------------
+	D3DXCreateTextureFromFile(pDevice,
+							  "data\\TEXTURE\\Title_blue.png",
+							  &s_pTexture);
+
+	//------------------------------
+	//	構造体の初期化
+	//------------------------------
+	for (int nCnt = 0; nCnt < MAX_RULE; nCnt++)
+	{
+		Rule *rule = s_Rule + nCnt;
+
+		rule->pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);		//位置
+		rule->fWidth = 0.0f;	//幅
+		rule->fHeight = 0.0f;	//高さ
+		rule->bUse = false;
+	}
+
+	//頂点バッファの生成
+	pDevice->CreateVertexBuffer(sizeof(VERTEX_2D) * 4 * MAX_RULE,	//確保するバッファのサイズ
+								D3DUSAGE_WRITEONLY,
+								FVF_VERTEX_2D,			//頂点フォーマット
+								D3DPOOL_MANAGED,
+								&s_pVtxBuff,
+								NULL);
+
+	VERTEX_2D*pVtx;		//頂点情報へのポインタ
+
+	//頂点バッファをロックし、頂点情報へのポインタを取得
+	s_pVtxBuff->Lock(0, 0, (void**)&pVtx, 0);
+
+	//------------------------------
+	//	頂点情報の設定
+	//------------------------------
+	for (int nCnt = 0; nCnt < MAX_RULE; nCnt++)
+	{
+		//頂点座標の設定
+		pVtx[0].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		pVtx[1].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		pVtx[2].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		pVtx[3].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+
+		//rhwの設定
+		pVtx[0].rhw = 1.0f;
+		pVtx[1].rhw = 1.0f;
+		pVtx[2].rhw = 1.0f;
+		pVtx[3].rhw = 1.0f;
+
+		//頂点カラーの設定
+		pVtx[0].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+		pVtx[1].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+		pVtx[2].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+		pVtx[3].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+
+		//テクスチャ座標の設定
+		pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
+		pVtx[1].tex = D3DXVECTOR2(1.0f, 0.0f);
+		pVtx[2].tex = D3DXVECTOR2(0.0f, 1.0f);
+		pVtx[3].tex = D3DXVECTOR2(1.0f, 1.0f);
+
+		pVtx += 4;
+	}
+
+	//頂点バッファをアンロックする
+	s_pVtxBuff->Unlock();
+}
+
+//============================
+// ルール選択画面の終了
+//============================
+void UninitRule(void)
+{
+	//テクスチャの破棄
+	if (s_pTexture != NULL)
+	{
+		s_pTexture->Release();
+		s_pTexture = NULL;
+	}
+
+	//頂点バッファの破壊
+	if (s_pVtxBuff != NULL)
+	{
+		s_pVtxBuff->Release();
+		s_pVtxBuff = NULL;
+	}
+}
+
+//============================
+// ルール選択画面の更新
+//============================
+void UpdateRule(void)
+{
+
+}
+
+//============================
+// ルール選択画面の描画
+//============================
+void DrawRule(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();	//デバイスの取得
+
+	//頂点バッファをデータストリームに設定
+	pDevice->SetStreamSource(0, s_pVtxBuff, 0, sizeof(VERTEX_2D));
+
+	//頂点フォーマットの設定
+	pDevice->SetFVF(FVF_VERTEX_2D);
+
+	for (int nCnt = 0; nCnt < MAX_RULE; nCnt++)
+	{
+		Rule *rule = s_Rule + nCnt;
+
+		if (rule->bUse == true)
+		{
+			//テクスチャの設定
+			pDevice->SetTexture(0, s_pTexture);
+
+			//リザルトの描画
+			pDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP,		//プリミティブの種類
+				nCnt * 4,						//描画する最初の頂点インデックス
+				2);						//描画するプリミティブ数
+		}
+	}
+}
+
+//============================
+// ルール選択画面の設定
+//============================
+void SetRule(D3DXVECTOR3 pos)
+{
+	VERTEX_2D*pVtx;		//頂点情報へのポインタ
+
+	//頂点バッファをロックし、頂点情報へのポインタを取得
+	s_pVtxBuff->Lock(0, 0, (void**)&pVtx, 0);
+
+	for (int nCnt = 0; nCnt < MAX_RULE; nCnt++)
+	{
+		Rule *rule = s_Rule + nCnt;
+
+		if (rule->bUse == false)
+		{
+			//構造体の設定
+			rule->pos = pos;
+			rule->fWidth = 50.0f;
+			rule->fHeight = 25.0f;
+			rule->bUse = true;
+
+			//頂点座標の設定
+			pVtx[0].pos = rule->pos + D3DXVECTOR3(-rule->fWidth, rule->fHeight, 0.0f);
+			pVtx[1].pos = rule->pos + D3DXVECTOR3(rule->fWidth, rule->fHeight, 0.0f);
+			pVtx[2].pos = rule->pos + D3DXVECTOR3(-rule->fWidth, -rule->fHeight, 0.0f);
+			pVtx[3].pos = rule->pos + D3DXVECTOR3(rule->fWidth, -rule->fHeight, 0.0f);
+
+			break;
+		}
+		pVtx += 4;
+	}
+
+	//頂点バッファをアンロックする
+	s_pVtxBuff->Unlock();
+}
