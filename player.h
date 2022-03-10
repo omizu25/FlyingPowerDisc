0acf5b3282f07650b11d38caeb92b55d57f757f5
浜田琉雅 <211035@st.yoshida-g.ac.jp>
2022-03-10 09:03:03 +0900

    Playerにステータスの概念をつけたよ

--- player.h
+++ player.h
@@ -9,7 +9,7 @@
 #define PLAYERMOVE (50)  //ループの幅
 #define PLAYERSIZ_X (100)//サイズ
 #define PLAYERSIZ_Y (100)//サイズ
-#define NUM_PL (1)
+
 
 typedef enum
 {
@@ -23,40 +23,31 @@ typedef enum
 	PLAYESTATE_MAX
 
 }PLAYESTATE;
-typedef enum
-{
-	PLAYEROTSTATE_NORMAL = 0,
-	PLAYEROTSTATE_ROTATE,		//回転
-	PLAYEROTSTATE_MAX
 
-}PLAYEROTSTATE;
-typedef enum
-{
-	JUMP_NORMAL = 0,
-	JUMP_NOU,
-	JUMP_MAX,
 
-}JUMP;
 typedef struct
 {
 	D3DXVECTOR3 pos;			//位置
 	D3DXVECTOR3 posOld;			//前回の位置
 	D3DXVECTOR3 move;			//前回の位置
 	PLAYESTATE state;			//状態
-	PLAYEROTSTATE rotstate;		//状態
-	int Speed;					//速度
 	int nLife;					//寿命
 	int nType;					//タイプ
 	float fheight;				//高さ
 	float fwidth;				//幅
-	float MapMove;              //マップをどれだけ走ったか
+	float Pow;					//パワー
+	float Speed;				//スピード
 	bool bUse;					//使用してるかどうか
 }Player;
+
 //プロトタイプ宣言
 void InitPlayer(void);
 void UninitPlayer(void);
 void UpdatePlayer(void);
 void DrawPlayer(void);
+void SetPlayer(D3DXVECTOR3 pos, int nType, bool light);
+void SetTex2d(VERTEX_2D *pVtx, float left, float right, float top, float down);
+
 void SetUp(VERTEX_2D *pVtx,
 	float posx,		//中心点ｘ
 	float posy,		//中心点ｙ
