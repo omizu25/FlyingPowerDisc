a4def8172d91c2de51b5dc6c4ffb0dacadf2b356
浜田琉雅 <211035@st.yoshida-g.ac.jp>
2022-03-11 14:37:01 +0900

    かべｃｐｐ６割完成 反射大体できた

--- wall.h
+++ wall.h
@@ -10,21 +10,32 @@
 #define WALLSIZ_X (100)//サイズ
 #define WALLSIZ_Y (100)//サイズ
 
+
+
+typedef enum
+{
+	WALL_UP = 0,
+	WALL_DOWN,
+	WALL_LEFT,
+	WALL_LIGHT,
+	WALL_MAX
+}WALL;
+
 //プロトタイプ宣言
 void InitWall(void);
 void UninitWall(void);
 void UpdateWall(void);
 void DrawWall(void);
-bool CollisionWall(D3DXVECTOR3 *pPos, D3DXVECTOR3 *pPosOld, D3DXVECTOR3 *move);
-bool CalcParticlePlaneAfterPos(
-	D3DXVECTOR3 *pColliPos,
-	D3DXVECTOR3 *pVelo,
-	FLOAT res,
-	FLOAT time,
-	D3DXVECTOR3 *pOut_pos,
-	D3DXVECTOR3 *pOut_velo
-);
-void MoveWall(void);
+bool CollisionWall(D3DXVECTOR3 *pPos, D3DXVECTOR3 *pPosOld);
+//bool CalcParticlePlaneAfterPos(
+//	D3DXVECTOR3 *pColliPos,
+//	D3DXVECTOR3 *pVelo,
+//	FLOAT res,
+//	FLOAT time,
+//	D3DXVECTOR3 *pOut_pos,
+//	D3DXVECTOR3 *pOut_velo
+//);
+//void MoveWall(void);
 //Wall* GetWall(void);
 #endif
 
