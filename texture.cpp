5403be0e4b689e4e3265589cfcb867ec151783e9
Sato Teruto <211052@st.yoshida-g.ac.jp>
2022-03-24 12:25:16 +0900

    赤い光と青い光のテクスチャ追加

--- texture.cpp
+++ texture.cpp
@@ -24,7 +24,9 @@ const char *s_FileName[] =
 	"data/TEXTURE/TITLE/BG.png",				// タイトル画面の背景(平原)
 	"data/TEXTURE/TITLE/BG2.png",				// タイトル画面の背景(雪原)
 	"data/TEXTURE/TITLE/BG3.png",				// タイトル画面の背景(海)
-	"data/TEXTURE/TITLE/Title_Rogo.png",		// タイトルロゴ
+	"data/TEXTURE/TITLE/Title_Logo.png",		// タイトルロゴ
+	"data/TEXTURE/TITLE/Light_Blue",			// 青い光
+	"data/TEXTURE/TITLE/Light_Red",				// 赤い光
 	"data/TEXTURE/TITLE/Game_Start.png",		// ゲームスタートの文字
 	"data/TEXTURE/TITLE/Rule_Select.png",		// ルール選択の文字
 	"data/TEXTURE/TITLE/Map_Select.png",		// マップ選択の文字
